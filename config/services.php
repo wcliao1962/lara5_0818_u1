617b1fb94e78e4ae0eb61dcfd368aadd4ce5b4f9
廖文忠 <wcliao1962@gmail.com>
2017-12-21 22:44:19 +0800

    將由facebook, google, github所申請的ID加入.env & 並加入services.php當中

--- config/services.php
+++ config/services.php
@@ -34,5 +34,23 @@ return [
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
+    
+    'facebook' => [
+        'client_id' => env('FACEBOOK_API_ID'),
+        'client_secret' => env('FACEBOOK_API_SECRET'),
+        'redirect' => env('FACEBOOK_CALLBACK'),
+    ],
+
+    'google' => [
+        'client_id'     => env('GOOGLE_API_ID'),
+        'client_secret' => env('GOOGLE_API_SECRET'),
+        'redirect'      => env('GOOGLE_CALLBACK')
+    ],
+
+    'github' => [
+        'client_id'     => env('GITHUB_API_ID'),
+        'client_secret' => env('GITHUB_API_SECRET'),
+        'redirect'      => env('GITHUB_CALLBACK')
+    ],
 
 ];
